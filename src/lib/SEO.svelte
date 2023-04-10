5f31dbb365e55d81c685f49c7369dfd44b226045
David D Lawson <hello@ddlawson.com>
2023-04-10 15:59:41 +0100

    Add umami analytics & social share images

--- src/lib/SEO.svelte
+++ src/lib/SEO.svelte
@@ -11,14 +11,14 @@
     <meta property="og:url" content="https://justhiit.it/">
     <meta property="og:title" content="JustHIIT.it | High-Intensity Interval Training & Tabata Timer">
     <meta property="og:description" content="A simple, modern and user-friendly timer for High-Intensity Interval Training & Tabata workouts. Set the number of sets, work time, and rest time to start your workout.">
-    <meta property="og:image" content="https://justhiit.it/images/og-image.jpg">
+    <meta property="og:image" content="https://justhiit.it/JustHIITit_Social_Share.png">
   
     <!-- Twitter -->
     <meta property="twitter:card" content="summary_large_image">
     <meta property="twitter:url" content="https://justhiit.it/">
     <meta property="twitter:title" content="JustHIIT.it | High-Intensity Interval Training & Tabata Timer">
     <meta property="twitter:description" content="A simple, modern and user-friendly timer for High-Intensity Interval Training & Tabata workouts. Set the number of sets, work time, and rest time to start your workout.">
-    <meta property="twitter:image" content="https://yourwebsite.com/images/twitter-image.jpg">
+    <meta property="twitter:image" content="https://justhiit.it/JustHIITit_Social_Share.png">
 
     <script async defer data-website-id="402dc1d7-1b95-4e23-b10c-fc0942a8419e" src="https://analytics.ddlawson.com/umami.js"></script>
 </svelte:head>
