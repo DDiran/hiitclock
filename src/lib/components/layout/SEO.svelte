73807625eb05e4f81cad04cf0eb74837b88b6cdd
David D Lawson <hello@ddlawson.com>
2023-04-18 16:31:48 +0100

    Refactor SEO component, remove analytics from dev

--- src/lib/components/layout/SEO.svelte
+++ src/lib/components/layout/SEO.svelte
@@ -1,24 +1,33 @@
-<script lang="ts"></script>
+<script lang="ts">
+import { TITLE, DESCRIPTION, KEYWORDS, AUTHOR } from "$lib/constants";
+
+let title = TITLE;
+if (import.meta.env.NODE_ENV !== "production") {
+  title = `Dev | ${title}`;
+}
+
+const umamiAnalytics =
+  import.meta.env.NODE_ENV === "production"
+    ? `
+        async
+        defer
+        data-website-id="402dc1d7-1b95-4e23-b10c-fc0942a8419e"
+        src="https://analytics.ddlawson.com/umami.js"
+    `
+    : "";
+</script>
 
 <svelte:head>
-  <title>JustHIIT.it | High-Intensity Interval Training & Tabata Timer</title>
-  <meta
-    name="description"
-    content="A simple, modern and user-friendly timer for High-Intensity Interval Training & Tabata workouts. Set the number of sets, work time, and rest time to start your workout." />
-  <meta
-    name="keywords"
-    content="HIIT, High-Intensity Interval Training, Workout, Tabata, Timer, Fitness, Exercise, Training" />
-  <meta name="author" content="David D Lawson" />
+  <title>{title}</title>
+  <meta name="description" content={DESCRIPTION} />
+  <meta name="keywords" content={KEYWORDS} />
+  <meta name="author" content={AUTHOR} />
 
   <!-- Open Graph / Facebook -->
   <meta property="og:type" content="website" />
   <meta property="og:url" content="https://justhiit.it/" />
-  <meta
-    property="og:title"
-    content="JustHIIT.it | High-Intensity Interval Training & Tabata Timer" />
-  <meta
-    property="og:description"
-    content="A simple, modern and user-friendly timer for High-Intensity Interval Training & Tabata workouts. Set the number of sets, work time, and rest time to start your workout." />
+  <meta property="og:title" content={title} />
+  <meta property="og:description" content={DESCRIPTION} />
   <meta
     property="og:image"
     content="https://justhiit.it/JustHIITit_Social_Share.png" />
@@ -26,19 +35,11 @@
   <!-- Twitter -->
   <meta property="twitter:card" content="summary_large_image" />
   <meta property="twitter:url" content="https://justhiit.it/" />
-  <meta
-    property="twitter:title"
-    content="JustHIIT.it | High-Intensity Interval Training & Tabata Timer" />
-  <meta
-    property="twitter:description"
-    content="A simple, modern and user-friendly timer for High-Intensity Interval Training & Tabata workouts. Set the number of sets, work time, and rest time to start your workout." />
+  <meta property="twitter:title" content={title} />
+  <meta property="twitter:description" content={DESCRIPTION} />
   <meta
     property="twitter:image"
     content="https://justhiit.it/JustHIITit_Social_Share.png" />
 
-  <script
-    async
-    defer
-    data-website-id="402dc1d7-1b95-4e23-b10c-fc0942a8419e"
-    src="https://analytics.ddlawson.com/umami.js"></script>
+  {@html `<script ${umamiAnalytics}></script>`}
 </svelte:head>
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,11 @@
 export const DEFAULT_SETS = 15;
 export const DEFAULT_WORK_TIME = 40;
 export const DEFAULT_REST_TIME = 20;
+
+export const TITLE =
+  "JustHIIT.it | High-Intensity Interval Training & Tabata Timer";
+export const DESCRIPTION =
+  "A simple, modern and user-friendly timer for High-Intensity Interval Training & Tabata workouts. Set the number of sets, work time, and rest time to start your workout.";
+export const AUTHOR = "David D Lawson";
+export const KEYWORDS =
+  "HIIT, High-Intensity Interval Training, Workout, Tabata, Timer, Fitness, Exercise, Training";
