edf967f6c262d6e2d26120c854569e18c8869c14
David D Lawson <hello@ddlawson.com>
2023-04-23 19:50:12 +0100

    Prettify with basic SVG background

---
+++ src/lib/components/layout/BackgroundPattern.svelte
@@ -0,0 +1,7 @@
+<script>
+export let className = "";
+</script>
+
+<div
+  class="fixed top-0 bg-center w-full h-full -z-10 bg-no-repeat {className}"
+  style="background-image: url('data:image/svg+xml,%3Csvg xmlns=\'http://www.w3.org/2000/svg\' viewBox=\'0 0 100 100\'%3E%3Ccircle cx=\'50\' cy=\'50\' r=\'25\' fill=\'rgba(255, 255, 255, 0.1)\' /%3E%3Ccircle cx=\'25\' cy=\'25\' r=\'15\' fill=\'rgba(255, 255, 255, 0.15)\' /%3E%3Ccircle cx=\'75\' cy=\'25\' r=\'15\' fill=\'rgba(255, 255, 255, 0.15)\' /%3E%3Ccircle cx=\'25\' cy=\'75\' r=\'15\' fill=\'rgba(255, 255, 255, 0.15)\' /%3E%3Ccircle cx=\'75\' cy=\'75\' r=\'15\' fill=\'rgba(255, 255, 255, 0.15)\' /%3E%3Ccircle cx=\'40\' cy=\'20\' r=\'10\' fill=\'rgba(255, 255, 255, 0.1)\' /%3E%3Ccircle cx=\'80\' cy=\'60\' r=\'20\' fill=\'rgba(255, 255, 255, 0.1)\' /%3E%3Ccircle cx=\'20\' cy=\'50\' r=\'5\' fill=\'rgba(255, 255, 255, 0.15)\' /%3E%3Ccircle cx=\'60\' cy=\'80\' r=\'10\' fill=\'rgba(255, 255, 255, 0.15)\' /%3E%3C/svg%3E');" />
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,11 +15,14 @@ import { fade } from "svelte/transition";
 import Seo from "$lib/components/layout/SEO.svelte";
 import Navbar from "$lib/components/layout/Navbar.svelte";
 import Footer from "$lib/components/layout/Footer.svelte";
+import BackgroundPattern from "$lib/components/layout/BackgroundPattern.svelte";
 </script>
 
 <Seo />
 
 <Navbar />
+
+<BackgroundPattern className="opacity-20" />
 
 <main transition:fade>
   <slot />
